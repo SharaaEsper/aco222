91037d51199b14c1d8492d4b03141c06ead95ae9
Ryuujinx <admin@failverse.com>
2022-12-09 12:29:30 -0600

    part1

---
+++ day8/part1.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+
+input = Array.new
+File.readlines('input').each do |line|
+  input << line
+end
+
+x_pos = 0
+y_pos = 0
+forest = Hash.new
+#0,0 is bottom left so we gotta flip it on its head. 
+input.reverse.each do |line|
+  x_pos = 0
+  line.strip.split("").each do |cha|
+    forest["#{x_pos},#{y_pos}"] = cha
+    x_pos += 1
+  end
+  y_pos += 1
+end
+
+#This sets the largest x/y so we don't try to find trees in nil-land. 
+y_limit = forest.keys.select { |s| s.match(/^0,\d+/) }[-1].gsub(/\d+,/, '').strip.to_i
+x_limit = forest.keys.select { |s| s.match(/^\d+,0/) }[-1].gsub(/,\d+/, '').strip.to_i
+
+#Biggest Tree is 9. 
+tall_boi = 9
+
+visible_trees = Hash.new
+0.upto(x_limit) do |x_pos|
+  0.upto(y_limit) do |y_pos|
+    if x_pos == 0 || y_pos == 0 || x_pos == x_limit || y_pos == y_limit
+      visible_trees["#{x_pos},#{y_pos}"] = forest["#{x_pos},#{y_pos}"]
+    else
+      tallest = true
+      #To the left
+      (x_pos -1).downto(0) do |x_pos_scan|
+        if forest["#{x_pos_scan},#{y_pos}"] >= forest["#{x_pos},#{y_pos}"]
+          tallest = false
+          break
+        end
+      end
+      if tallest
+        visible_trees["#{x_pos},#{y_pos}"] = forest["#{x_pos},#{y_pos}"]
+        next
+      else
+        tallest = true
+      end
+
+      #To the right
+      (x_pos +1).upto(x_limit) do |x_pos_scan|
+        if forest["#{x_pos_scan},#{y_pos}"] >= forest["#{x_pos},#{y_pos}"]
+          tallest = false
+          break
+        end
+      end
+      if tallest
+        visible_trees["#{x_pos},#{y_pos}"] = forest["#{x_pos},#{y_pos}"]
+        next
+      else
+        tallest = true
+      end
+
+      #To the...down
+      (y_pos -1).downto(0) do |y_pos_scan|
+        if forest["#{x_pos},#{y_pos_scan}"] >= forest["#{x_pos},#{y_pos}"]
+          tallest = false
+          break
+        end
+      end
+      if tallest
+        visible_trees["#{x_pos},#{y_pos}"] = forest["#{x_pos},#{y_pos}"]
+        next
+      else
+        tallest = true
+      end
+
+      #And up.
+      (y_pos +1).upto(y_limit) do |y_pos_scan|
+        if forest["#{x_pos},#{y_pos_scan}"] >= forest["#{x_pos},#{y_pos}"]
+          tallest = false
+          break
+        end
+      end
+      if tallest
+        visible_trees["#{x_pos},#{y_pos}"] = forest["#{x_pos},#{y_pos}"]
+        next
+      else
+        tallest = true
+      end                   
+    end
+  end
+end
+
+puts visible_trees.keys.count
